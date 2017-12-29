d06bce00a48d65ec21563ca3b8d593fdf0760dca
Derek Lin <derek.lin@blackrock.com>
2017-12-29 10:06:56 -0800

    wildcard route with lazy module not matching

--- apps/app1/src/app/core/core-routing.module.ts
+++ apps/app1/src/app/core/core-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Feature1Component } from '../feature1/feature1.component';
 import { Feature2Component } from '../feature2/feature2.component';
+import { NotFoundComponent, NotFoundModule } from '@nx-demo-jest/not-found';
 
 const routes: Routes = [
   {
@@ -12,16 +13,22 @@ const routes: Routes = [
       { path: '', redirectTo: 'f1', pathMatch: 'full' },
       { path: 'f1', component: Feature1Component },
       { path: 'f2', component: Feature2Component },
+      { path: 'not-found', loadChildren: '@nx-demo-jest/not-found/src/not-found.module#NotFoundModule' },
       {
         path: '**',
-        loadChildren: '@nx-demo-jest/not-found/src/not-found.module#NotFoundModule'
-      }
+        // component: NotFoundComponent // this works
+        // loadChildren: '@nx-demo-jest/not-found/src/not-found.module#NotFoundModule' // this doesn't
+        redirectTo: 'not-found', pathMatch: 'full' // the root (/) redirects to non-found
+      },
     ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    NotFoundModule
+  ],
   exports: [RouterModule]
 })
 export class CoreRoutingModule {}
--- libs/not-found/index.ts
+++ libs/not-found/index.ts
@@ -1,1 +1,2 @@
 export { NotFoundModule } from './src/not-found.module';
+export { NotFoundComponent } from './src/not-found/not-found.component';
