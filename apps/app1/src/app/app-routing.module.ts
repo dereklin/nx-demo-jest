018e7c2b052cc04f737ca9993717fd8dc3b8e114
Derek Lin <dereklin@hotmail.com>
2017-12-31 10:00:44 -0800

    code format

--- apps/app1/src/app/app-routing.module.ts
+++ apps/app1/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
 
-import { CoreModule } from './core/core.module';
-import { Feature3Component } from './feature3/feature3.component';
-import { Feature3Module } from './feature3/feature3.module';
+import { CoreModule } from "./core/core.module";
+import { Feature3Component } from "./feature3/feature3.component";
+import { Feature3Module } from "./feature3/feature3.module";
 
-const routes: Route[] = [{ path: 'f3', component: Feature3Component }];
+const routes: Route[] = [{ path: "f3", component: Feature3Component }];
 @NgModule({
   imports: [RouterModule.forRoot(routes), CoreModule, Feature3Module],
   exports: [RouterModule]
