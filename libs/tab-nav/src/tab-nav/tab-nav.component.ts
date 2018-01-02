794e1b711846bd05ff2a96c9b9dadbfe265b0f17
Derek Lin <dereklin@hotmail.com>
2018-01-01 23:12:10 -0800

    trying out scss

--- libs/tab-nav/src/tab-nav/tab-nav.component.ts
+++ libs/tab-nav/src/tab-nav/tab-nav.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'my-tab-nav',
   templateUrl: './tab-nav.component.html',
-  styleUrls: ['./tab-nav.component.css']
+  styleUrls: ['./tab-nav.component.scss']
 })
 export class TabNavComponent implements OnInit {
   @Input() public tabs: Tab[];
