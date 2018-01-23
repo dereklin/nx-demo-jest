bfb2ba75a3dc743546e1af2285d9823816d5c518
Derek Lin <derek.lin@blackrock.com>
2018-01-23 13:08:23 -0800

    add some dummy code to test format

--- apps/app1/src/app/feature1/feature1.component.ts
+++ apps/app1/src/app/feature1/feature1.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feature1.component.css']
 })
 export class Feature1Component implements OnInit {
-  constructor() {}
+  private dummy = {
+    type1: { dummyPropertyFunction: undefined },
+    type2: { dummyPropertyFunction: undefined },
+    type3: { dummyPropertyFunction: undefined },
+    type4: { dummyPropertyFunction: undefined },
+    type5: { dummyPropertyFunction: undefined },
+    type6: { dummyPropertyFunction: undefined },
+    type7: { dummyPropertyFunction: undefined },
+    type8: { dummyPropertyFunction: undefined },
+  };
 
-  public ngOnInit() {}
+  constructor() {
+    this.dummy.type1.dummyPropertyFunction
+      = this.dummy.type2.dummyPropertyFunction
+      = this.dummy.type3.dummyPropertyFunction
+      = this.dummy.type4.dummyPropertyFunction
+      = this.dummy.type5.dummyPropertyFunction
+      = this.dummy.type6.dummyPropertyFunction
+      = this.dummy.type7.dummyPropertyFunction
+      = this.dummy.type8.dummyPropertyFunction
+      = () => {
+        return 'dummy';
+      };
+  }
+
+  public ngOnInit() { }
 }
