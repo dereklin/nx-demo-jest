ecf4761f52d1b08bfbfc766b7de87c5824fc4e3b
Derek Lin <derek.lin@blackrock.com>
2018-01-26 14:14:32 -0800

    try to make story book work

---
+++ stories/tab-nav.stories.ts
@@ -0,0 +1,30 @@
+import { storiesOf } from '@storybook/angular';
+import { withNotes } from '@storybook/addon-notes';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { RouterModule } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { TabNavComponent } from '../libs/tab-nav';
+
+import { Dummy1Service } from '../libs/dummy1-service';
+
+const COREROUTEBASESPACE = 'go/';
+
+storiesOf('my-top-nav', module).add('basic', () => ({
+  moduleMetadata: {
+    imports: [],
+    schemas: [NO_ERRORS_SCHEMA],
+    declarations: [],
+    providers: []
+  },
+  component: TabNavComponent,
+  props: {
+    tabs: [
+      { value: ``, display: 'Home' },
+      { value: `${COREROUTEBASESPACE}f1`, display: 'Feature 1' },
+      { value: `${COREROUTEBASESPACE}f2`, display: 'Feature 2' },
+      { value: 'f3', display: 'Feature 3' }
+    ]
+  }
+}));
